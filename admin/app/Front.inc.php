cc00835ef0288fa3c54adf6ea6972c6a09249565
1569501393@qq.com <1569501393@qq.com>
2018-01-30 00:04:03 +0800

    #商品列表接口

--- admin/app/Front.inc.php
+++ admin/app/Front.inc.php
@@ -3,65 +3,81 @@
 /******** 控制器 ********/
 class FrontController extends Controller
 {
-	// 重载了 Controller 的 BeforeRun 方法
-	function BeforeRun()
-	{
-		global $quoteData, $AccessControl, $__GroupSession;
+    // 重载了 Controller 的 BeforeRun 方法
+    function BeforeRun()
+    {
+        global $quoteData, $AccessControl, $__GroupSession;
 
-		$firstModule = current( GetExplode( '.', $this->module ) );
-        // var_dump('TODO jieqiangtest==$firstModule==',$firstModule,debug_backtrace());exit;
-		$freeModule = array( 'login', 'tool.upload', 'product.upload.image', 'product.attribute.upload' );
+        $firstModule = current(GetExplode('.', $this->module));
 
-		if ( !$firstModule )
-		{
-			Redirect( '?mod=index' );
-		}
+        $freeModule = array('login', 'tool.upload', 'product.upload.image', 'product.list', 'product.attribute.upload');
 
-		// 权限管理
-		$inAccess = $AccessControl->InModule( $this->module, $this->action );
+        if (!$firstModule) {
+            Redirect('?mod=index');
+        }
 
-		$session = Common::GetSession();
+        // 权限管理
+        $inAccess = $AccessControl->InModule($this->module, $this->action);
 
-		if ( !$session && $inAccess )
-		{
-			Redirect( '?mod=login' );
-		}
+        $session = Common::GetSession();
 
-		$AdminModel = Core::ImportModel( 'Admin' );
+        // 如果是api且 http://erp.jieqiangtec.cn/admin/?mod=product.list
+        // var_dump('TODO jieqiangtest==$firstModule==',$this->module,$inAccess,Common::GetSession(),debug_backtrace());exit;
+//        if ($this->module == 'product.list' && $_GET['api'] = 1) {
+        if ($this->module == 'product.list' ) {
+            $session = 1;
+        }
 
-		// TODO jieqiangtest 检查用户信息
-		$adminInfo = $AdminModel->GetAdministrator( $session['user_id'] );
+        if (!$session && $inAccess) {
+            Redirect('?mod=login');
+        }
 
-		//if ( !$adminInfo && !in_array( $this->module, $freeModule ) )
-		if ( !$adminInfo && $inAccess )
-		{
-			Redirect( '?mod=login' );
-		}
 
-		$userGroup = $adminInfo['user_group'];
+        $AdminModel = Core::ImportModel('Admin');
 
-		$groupInfo = $AdminModel->GetAdministratorGroup( $userGroup );
 
-		if ( !$groupInfo && $inAccess )
-			Common::ALert( '尚未加入管理组' );
+        // 如果是api且 http://erp.jieqiangtec.cn/admin/?mod=product.list
+        // var_dump('TODO jieqiangtest==$firstModule==',$this->module,$inAccess,Common::GetSession(),debug_backtrace());exit;
+//        if ($this->module == 'product.list' && $_GET['api'] = 1) {
+        if ($this->module == 'product.list' ) {
+            $adminInfo = 1;
+        }else{
+            // TODO jieqiangtest 检查用户信息
+            $adminInfo = $AdminModel->GetAdministrator($session['user_id']);
+        }
 
-		$__GroupSession = $groupInfo;
+        //if ( !$adminInfo && !in_array( $this->module, $freeModule ) )
+        if (!$adminInfo && $inAccess) {
+            Redirect('?mod=login');
+        }
 
-		$AccessControl->SetAllow( explode( ',', $groupInfo['allow'] ) );
 
-		if ( !$AccessControl->ToTest( $this->module, $this->action ) )
-		{
-			if ( $session )
-			{
-				header( "Content-Type:text/html; charset=utf-8" );
-				exit( '权限不足,不允许访问' );
-			}
-			else
-			{
-				Redirect( '?mod=login' );
-			}
-		}
-	}
+        // 如果是api且 http://erp.jieqiangtec.cn/admin/?mod=product.list
+        // var_dump('TODO jieqiangtest==$firstModule==',$this->module,$inAccess,Common::GetSession(),debug_backtrace());exit;
+//        if ($this->module == 'product.list' && $_GET['api'] = 1) {
+        if ($this->module == 'product.list' ) {
+            $groupInfo = 1;
+        }else{
+            $userGroup = $adminInfo['user_group'];
+            $groupInfo = $AdminModel->GetAdministratorGroup($userGroup);
+            if (!$groupInfo && $inAccess)
+                Common::ALert('尚未加入管理组');
+
+            $__GroupSession = $groupInfo;
+
+            $AccessControl->SetAllow(explode(',', $groupInfo['allow']));
+
+            if (!$AccessControl->ToTest($this->module, $this->action)) {
+                if ($session) {
+                    header("Content-Type:text/html; charset=utf-8");
+                    exit('权限不足,不允许访问');
+                } else {
+                    Redirect('?mod=login');
+                }
+            }
+
+        }
+    }
 }
 
 ?>
